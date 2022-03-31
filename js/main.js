1d04f986e647630f5258b17cdacf48d6a420e090
sabmile <sab288@yandex.ru>
2022-03-31 16:33:08 +0600

    добавлен модуль загрузки #upload-file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import {generateData} from './data.js';
 import {renderPictures} from './pictures.js';
 import {makeGalleryInteractive} from './gallery.js';
+import {uploadFile} from './uploadFile.js';
 
 const pictures = generateData(25);
 renderPictures(pictures);
 makeGalleryInteractive(pictures);
+uploadFile();
